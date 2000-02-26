c722d0e4c07f076a12993fd00860d6461dc8e19b
jll <jll>
2000-02-26 11:21:43 +0000

    reformated by cperl-mode (I think!)

--- Tangram/Cursor.pm
+++ Tangram/Cursor.pm
@@ -7,289 +7,289 @@ use Carp;
 
 sub new
 {
-   my ($pkg, $storage, $target, $conn) = @_;
+	my ($pkg, $storage, $target, $conn) = @_;
 
-   confess unless $conn;
+	confess unless $conn;
 
 	my $implicit = ref $target ? $target->object : $storage->cursor_object($target);
 	$target = Tangram::CursorObject->copy($implicit);
 
-   my $self = {};
+	my $self = {};
 
-   $self->{-storage} = $storage;
-   $self->{-target} = $target->{class};
-   $self->{-stored} = $target;
-   $self->{-implicit} = $implicit;
-   $self->{-selects} = [];
-   $self->{-conn} = $conn;
+	$self->{-storage} = $storage;
+	$self->{-target} = $target->{class};
+	$self->{-stored} = $target;
+	$self->{-implicit} = $implicit;
+	$self->{-selects} = [];
+	$self->{-conn} = $conn;
 
-   bless $self, $pkg;
+	bless $self, $pkg;
 }
 
 sub select
 {
-   my $self = shift;
+	my $self = shift;
 
-   my %args;
+	my %args;
 
-   if (@_ > 1)
-   {
-      %args = @_;
-   }
-   else
-   {
-      $args{filter} = shift;
-   }
+	if (@_ > 1)
+	{
+		%args = @_;
+	}
+	else
+	{
+		$args{filter} = shift;
+	}
 
-   $self->{-order} = $args{order};
+	$self->{-order} = $args{order};
 	$self->retrieve( @{ $args{retrieve} } ) if exists $args{retrieve};
 
-   local $stored = $self->{-stored};
+	local $stored = $self->{-stored};
    
-   local %done = map { $_ => 1 } $stored->parts();
-   delete $done{ $stored->class };
+	local %done = map { $_ => 1 } $stored->parts();
+	delete $done{ $stored->class };
 
-   my $filter = Tangram::Filter->new( tight => 100, objects => Set::Object->new($stored) );
+	my $filter = Tangram::Filter->new( tight => 100, objects => Set::Object->new($stored) );
 
-   if (my $user_filter = $args{filter})
-   {
-      $filter->{expr} = $user_filter->{expr};
-      $filter->{objects}->insert($user_filter->{objects}->members);
+	if (my $user_filter = $args{filter})
+	{
+		$filter->{expr} = $user_filter->{expr};
+		$filter->{objects}->insert($user_filter->{objects}->members);
 		$filter->{objects}->remove($self->{-implicit});
-   }
+	}
 
-   $self->_select($self->{-target}, $filter);
+	$self->_select($self->{-target}, $filter);
 
-   return undef unless @{$self->{-selects}}; 
+	return undef unless @{$self->{-selects}}; 
 
-   my $select = shift @{$self->{-selects}};
-   return undef unless $select;
+	my $select = shift @{$self->{-selects}};
+	return undef unless $select;
 
-   my ($sql, @parts) = @$select;
-   $self->{parts} = \@parts;
-   $self->{-cursor} = $self->{-storage}->sql_cursor($sql, $self->{-conn});
+	my ($sql, @parts) = @$select;
+	$self->{parts} = \@parts;
+	$self->{-cursor} = $self->{-storage}->sql_cursor($sql, $self->{-conn});
 
-   return $self->next;
+	return $self->next;
 }
 
 sub _select
 {
-   my ($self, $class, $filter) = @_;
+	my ($self, $class, $filter) = @_;
 
-   return if exists $done{$class};
+	return if exists $done{$class};
 
-   $done{$class} = 1;
+	$done{$class} = 1;
 
-   my $storage = $self->{-storage};
-   my $schema = $storage->{schema};
-   my $classes = $schema->{classes};
-   my $classdef = $classes->class($class);
-   my $class2id = $storage->{class2id};
-   my $stored = $self->{-stored};
+	my $storage = $self->{-storage};
+	my $schema = $storage->{schema};
+	my $classes = $schema->{classes};
+	my $classdef = $classes->class($class);
+	my $class2id = $storage->{class2id};
+	my $stored = $self->{-stored};
 
-   if ($classdef->{specs} && @{$classdef->{specs}} || $classdef->{stateless})
-   {
-      my @shared = ( ($classdef->{abstract} ? () : $class), # concat lists
-         map { _select_shared($_, $classes) } @{$classdef->{specs}} );
+	if ($classdef->{specs} && @{$classdef->{specs}} || $classdef->{stateless})
+	{
+		my @shared = ( ($classdef->{abstract} ? () : $class), # concat lists
+					   map { _select_shared($_, $classes) } @{$classdef->{specs}} );
 
-      if (@shared)
-      {
-         my $cols = $stored->cols;
-         my $from = $filter->from;
-         my $cid = $stored->class_id_col;
+		if (@shared)
+		{
+			my $cols = $stored->cols;
+			my $from = $filter->from;
+			my $cid = $stored->class_id_col;
 
-         my $where = join ' AND ',
+			my $where = join ' AND ',
 				"$cid IN (" . join(', ', map { $storage->class_id($_) } @shared) . ')',
-				$filter->where;
+					$filter->where;
          
-         push @{$self->{-selects}}, [ $self->build_select($cols, $from, $where), $stored->parts ];
-      }
+			push @{$self->{-selects}}, [ $self->build_select($cols, $from, $where), $stored->parts ];
+		}
 
-      foreach my $spec (@{$classdef->{specs}})
-      {
-         $self->_select_unshared($spec, $filter);
-      }
-   }
+		foreach my $spec (@{$classdef->{specs}})
+		{
+			$self->_select_unshared($spec, $filter);
+		}
+	}
 
-   elsif (!$classdef->{abstract})
-   {
-      my $cols = $stored->cols;
-      my $from = $filter->from;
-      my $where = $filter->where;
-      push @{$self->{-selects}}, [ $self->build_select($cols, $from, $where), $stored->parts ];
-   }
+	elsif (!$classdef->{abstract})
+	{
+		my $cols = $stored->cols;
+		my $from = $filter->from;
+		my $where = $filter->where;
+		push @{$self->{-selects}}, [ $self->build_select($cols, $from, $where), $stored->parts ];
+	}
 }
 
 sub _select_shared
 {
-   my ($class, $classes) = @_;
+	my ($class, $classes) = @_;
 
-   return () if $done{$class};
+	return () if $done{$class};
 
-   my $classdef = $classes->class($class);
-   return () unless $classdef->{stateless} && @{ $classdef->{bases} } <= 1;
+	my $classdef = $classes->class($class);
+	return () unless $classdef->{stateless} && @{ $classdef->{bases} } <= 1;
 
-   $done{$class} = 1;
+	$done{$class} = 1;
 
-   ( ($classdef->{abstract} ? () : $class), # concat lists
-   map { _select_shared($_, $classes) } @{$classdef->{specs}} );
+	( ($classdef->{abstract} ? () : $class), # concat lists
+	  map { _select_shared($_, $classes) } @{$classdef->{specs}} );
 }
 
 sub _select_unshared
 {
-   my ($self, $class, $filter) = @_;
+	my ($self, $class, $filter) = @_;
 
-   my $classes = $self->{-storage}{schema}{classes};
-   my $classdef = $classes->{$class};
+	my $classes = $self->{-storage}{schema}{classes};
+	my $classdef = $classes->{$class};
 
-   if ($classdef->{stateless} && @{ $classdef->{bases} } <= 1)
-   {
-      foreach my $spec (@{$classdef->{specs}})
-      {
-         $self->_select_unshared($spec, $filter);
-      }
-   }
-   else
-   {
-      my $mark = $stored->mark();
+	if ($classdef->{stateless} && @{ $classdef->{bases} } <= 1)
+	{
+		foreach my $spec (@{$classdef->{specs}})
+		{
+			$self->_select_unshared($spec, $filter);
+		}
+	}
+	else
+	{
+		my $mark = $stored->mark();
       
-      $stored->push_spec($class) unless $classdef->{stateless};
+		$stored->push_spec($class) unless $classdef->{stateless};
 
-      my $bases = $classdef->{bases};
+		my $bases = $classdef->{bases};
 
-      if (@$bases > 1)
-      {
-         my $schema = $self->{-storage}{schema};
+		if (@$bases > 1)
+		{
+			my $schema = $self->{-storage}{schema};
 
-         foreach my $base ( @$bases )
-         {
-            next if $done{$base};
+			foreach my $base ( @$bases )
+			{
+				next if $done{$base};
 
-            $schema->visit_up( $base,
-               sub
-               {
-                  my $base = shift;
-                  $stored->push_spec( $base ) unless $classes->{$base}{stateless} || $done{$base};
-                  $done{$base} = 1;
-               } );
-         }
-      }
+				$schema->visit_up( $base,
+								   sub
+								   {
+									   my $base = shift;
+									   $stored->push_spec( $base ) unless $classes->{$base}{stateless} || $done{$base};
+									   $done{$base} = 1;
+								   } );
+			}
+		}
 
-      $self->_select($class, $filter);
+		$self->_select($class, $filter);
       
-      $stored->pop_spec($mark);
-   }
+		$stored->pop_spec($mark);
+	}
 }
 
 sub build_select
 {
-   my ($self, $cols, $from, $where) = @_;
+	my ($self, $cols, $from, $where) = @_;
 
-   if (my $retrieve = $self->{-retrieve})
-   {
-      $cols = join ', ', $cols, map { $_->{expr} } @$retrieve;
-   }
+	if (my $retrieve = $self->{-retrieve})
+	{
+		$cols = join ', ', $cols, map { $_->{expr} } @$retrieve;
+	}
 
-   my $select = "SELECT $cols\n\tFROM $from\n\t" . ($where && "WHERE $where");
+	my $select = "SELECT $cols\n\tFROM $from\n\t" . ($where && "WHERE $where");
 
-   if (my $order = $self->{-order})
-   {
-      $select .= "\n\tORDER BY " . join ', ', map { $_->{expr} } @$order;
-   }
+	if (my $order = $self->{-order})
+	{
+		$select .= "\n\tORDER BY " . join ', ', map { $_->{expr} } @$order;
+	}
 
-   return $select;
+	return $select;
 }
 
 sub _next
 {
-   my ($self) = @_;
+	my ($self) = @_;
 
-   $self->{-current} = undef;
-   my @row;
+	$self->{-current} = undef;
+	my @row;
 
-   while (1)
-   {
-      @row = $self->{-cursor}->fetchrow;
-      last if @row;
+	while (1)
+	{
+		@row = $self->{-cursor}->fetchrow;
+		last if @row;
 
-      my $select = shift @{$self->{-selects}};
+		my $select = shift @{$self->{-selects}};
       
-      unless ($select)
-      {
-         $self->{-cursor}->close();
-         return undef;
-      }
+		unless ($select)
+		{
+			$self->{-cursor}->close();
+			return undef;
+		}
 
-      $self->{-cursor}{statement}->finish;
+		$self->{-cursor}{statement}->finish;
 
-      my ($sql, @parts) = @$select;
-      $self->{parts} = \@parts;
-      $self->{-cursor} = $self->{-storage}->sql_cursor($sql, $self->{-conn});
-   }
+		my ($sql, @parts) = @$select;
+		$self->{parts} = \@parts;
+		$self->{-cursor} = $self->{-storage}->sql_cursor($sql, $self->{-conn});
+	}
 
-   my $id = shift @row;
-   my $storage = $self->{-storage};
+	my $id = shift @row;
+	my $storage = $self->{-storage};
 
-   my $classId = shift @row;
-   my $class = $storage->{id2class}{$classId} or die "unknown class id $classId";
+	my $classId = shift @row;
+	my $class = $storage->{id2class}{$classId} or die "unknown class id $classId";
 
-   # even if object is already loaded we must read it so that @rpw only contains residue
-   my $obj = $storage->read_object($id, $class, \@row, @{ $self->{parts} } );
+	# even if object is already loaded we must read it so that @rpw only contains residue
+	my $obj = $storage->read_object($id, $class, \@row, @{ $self->{parts} } );
 
 	$self->{-residue} = exists $self->{-retrieve}
 		? [ map { ref $_ ? $_->{type}->read_data(\@row) : shift @row } @{$self->{-retrieve}} ]
-		: \@row;
+			: \@row;
 
-   # if object is already loaded return previous copy
-   $obj = $storage->{objects}{$id} if exists $storage->{objects}{$id};
+	# if object is already loaded return previous copy
+	$obj = $storage->{objects}{$id} if exists $storage->{objects}{$id};
    
-   $self->{-current} = $obj;
+	$self->{-current} = $obj;
 
-   return $obj;
+	return $obj;
 }
 
 sub next
 {
-   my ($self) = @_;
+	my ($self) = @_;
 
-   return $self->_next unless wantarray;
+	return $self->_next unless wantarray;
 
-   my ($obj, @results);
+	my ($obj, @results);
 
-   while (defined($obj = $self->_next))
-   {
-      push @results, $obj;
-   }
+	while (defined($obj = $self->_next))
+	{
+		push @results, $obj;
+	}
 
-   return @results;
+	return @results;
 }
 
 sub current
 {
-   my ($self) = @_;
-   $self->{-current}
+	my ($self) = @_;
+	$self->{-current}
 }
 
 sub retrieve
 {
-   my $self = shift;
-   push @{$self->{-retrieve}}, @_;
+	my $self = shift;
+	push @{$self->{-retrieve}}, @_;
 }
 
 sub residue
 {
-   @{shift->{-residue}};
+	@{shift->{-residue}};
 }
 
 sub object
 {
-   my ($self) = @_;
-   return $self->{object};
+	my ($self) = @_;
+	return $self->{object};
 }
 
 sub close
 {
-   my ($self) = @_;
+	my ($self) = @_;
 	$self->{-cursor}->close();
 }
 
@@ -301,13 +301,13 @@ sub open
 {
 	my ($type, $storage, $select, $conn) = @_;
    
-   confess unless $conn;
+	confess unless $conn;
 	
-   bless
-   {
-      select => $select,
-      cursor => $storage->sql_cursor(substr($select->{expr}, 1, -1), $conn),
-   }, $type;
+	bless
+	{
+	 select => $select,
+	 cursor => $storage->sql_cursor(substr($select->{expr}, 1, -1), $conn),
+	}, $type;
 }
 
 sub fetchrow
@@ -321,20 +321,20 @@ sub fetchrow
 sub fetchall_arrayref
 {
 	my $self = shift;
-   my @results;
+	my @results;
 
 	while (my @row = $self->fetchrow)
-   {
-      push @results, [ @row ];
-   }
+	{
+		push @results, [ @row ];
+	}
 
-   return \@results;
+	return \@results;
 }
 
 sub new
 {
-   my $pkg = shift;
-   return bless [ @_ ] , $pkg;
+	my $pkg = shift;
+	return bless [ @_ ] , $pkg;
 }
 
 1;
