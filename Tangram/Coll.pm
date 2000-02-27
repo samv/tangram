f0bcf42bc5a229ce01d81bfd2750b6c5853492ab
jll <jll>
2000-02-27 09:25:17 +0000

    reformated by cperl-mode

--- Tangram/Coll.pm
+++ Tangram/Coll.pm
@@ -9,24 +9,24 @@ use base qw( Tangram::Type );
 
 sub members
 {
-   my ($self, $members) = @_;
-   keys %$members;
+	my ($self, $members) = @_;
+	keys %$members;
 }
 
 sub cols
 {
-   ()
+	()
 }
 
 sub read
 {
-   my ($self, $row, $obj, $members, $storage, $class) = @_;
+	my ($self, $row, $obj, $members, $storage, $class) = @_;
 
-   foreach my $member (keys %$members)
-   {
-      tie $obj->{$member}, 'Tangram::CollOnDemand',
+	foreach my $member (keys %$members)
+	{
+		tie $obj->{$member}, 'Tangram::CollOnDemand',
 			$self, $members->{$member}, $storage, $storage->id($obj), $member, $class;
-   }
+	}
 }
 
 sub bad_type
@@ -35,34 +35,46 @@ sub bad_type
     die "$item is not a '$class' in collection '$coll' of $obj";
 }
 
+sub set_load_state
+{
+	my ($self, $storage, $obj, $member, $state) = @_;
+	$storage->{scratch}{ref($self)}{$storage->id($obj)}{$member} = $state;
+}
+
+sub get_load_state
+{
+	my ($self, $storage, $obj, $member, $state) = @_;
+	return $storage->{scratch}{ref($self)}{$storage->id($obj)}{$member};
+}
+
 package Tangram::AbstractCollExpr;
 
 sub new
 {
-   my $pkg = shift;
-   bless [ @_ ], $pkg;
+	my $pkg = shift;
+	bless [ @_ ], $pkg;
 }
 
 sub exists
 {
-   my ($self, $expr, $filter) = @_;
-   my ($coll) = @$self;
+	my ($self, $expr, $filter) = @_;
+	my ($coll) = @$self;
 
-   if ($expr->isa('Tangram::QueryObject'))
-   {
-      $expr = Tangram::Select->new
-         (
-         cols => [ $expr->{id} ],
-         exclude => [ $coll ],
-         filter => $self->includes($expr)->and_perhaps($filter)
-         );
-   }
+	if ($expr->isa('Tangram::QueryObject'))
+	{
+		$expr = Tangram::Select->new
+			(
+			 cols => [ $expr->{id} ],
+			 exclude => [ $coll ],
+			 filter => $self->includes($expr)->and_perhaps($filter)
+			);
+	}
 
-   my $expr_str = $expr->{expr};
-   $expr_str =~ tr/\n/ /;
+	my $expr_str = $expr->{expr};
+	$expr_str =~ tr/\n/ /;
 
-   return Tangram::Filter->new( expr => "exists $expr_str", tight => 100,
-      objects => Set::Object->new( $expr->objects() ) );
+	return Tangram::Filter->new( expr => "exists $expr_str", tight => 100,
+								 objects => Set::Object->new( $expr->objects() ) );
 }
 
 package Tangram::CollExpr;
@@ -71,44 +83,44 @@ use base qw( Tangram::AbstractCollExpr );
 
 sub includes
 {
-   my ($self, $item) = @_;
-   my ($coll, $memdef) = @$self;
+	my ($self, $item) = @_;
+	my ($coll, $memdef) = @$self;
 
-   my $coll_tid = $coll->root_table;
+	my $coll_tid = $coll->root_table;
 
-   my $link_tid = Tangram::Alias->new;
-   my $coll_col = $memdef->{coll};
-   my $item_col = $memdef->{item};
+	my $link_tid = Tangram::Alias->new;
+	my $coll_col = $memdef->{coll};
+	my $item_col = $memdef->{item};
 
-   my $objects = Set::Object->new($coll, Tangram::LinkTable->new($memdef->{table}, $link_tid) ),
-   my $target;
+	my $objects = Set::Object->new($coll, Tangram::LinkTable->new($memdef->{table}, $link_tid) ),
+		my $target;
 
-   if (ref $item)
-   {
-      if ($item->isa('Tangram::QueryObject'))
-      {
-         $target = 't' . $item->object->root_table . '.id';
-         $objects->insert( $item->object );
-      }
-      else
-      {
-         $target = $coll->{storage}->id($item)
+	if (ref $item)
+	{
+		if ($item->isa('Tangram::QueryObject'))
+		{
+			$target = 't' . $item->object->root_table . '.id';
+			$objects->insert( $item->object );
+		}
+		else
+		{
+			$target = $coll->{storage}->id($item)
 				or die "'$item' is not a persistent object";
-      }
-   }
-   else
-   {
-      $target = $item;
-   }
+		}
+	}
+	else
+	{
+		$target = $item;
+	}
 
-   Tangram::Filter->new
-   (
-      expr => "t$link_tid.$coll_col = t$coll_tid.id AND t$link_tid.$item_col = $target",
-      tight => 100,      
-      objects => $objects,
-		link_tid => $link_tid # for Sequence prefetch
-   )
-}
+	Tangram::Filter->new
+			(
+			 expr => "t$link_tid.$coll_col = t$coll_tid.id AND t$link_tid.$item_col = $target",
+			 tight => 100,      
+			 objects => $objects,
+			 link_tid => $link_tid # for Sequence prefetch
+			)
+		}
 
 package Tangram::IntrCollExpr;
 
@@ -116,9 +128,9 @@ use base qw( Tangram::AbstractCollExpr );
 
 sub includes
 {
-   my ($self, $item) = @_;
-   my ($coll, $memdef) = @$self;
-   my $coll_tid = $coll->root_table;
+	my ($self, $item) = @_;
+	my ($coll, $memdef) = @$self;
+	my $coll_tid = $coll->root_table;
 	my $item_class = $memdef->{class};
 	my $storage = $coll->{storage};
 
@@ -128,15 +140,15 @@ sub includes
 	{
 		if ($item->isa('Tangram::QueryObject'))
 		{
-		   my $item_tid = $item->object->table($item_class);
+			my $item_tid = $item->object->table($item_class);
 
 			return Tangram::Filter->new
-			(
-				expr => "t$item_tid.$memdef->{coll} = t$coll_tid.id",
-				tight => 100,
-				objects => Set::Object->new($coll, $item->object),
-			)
-		}
+				(
+				 expr => "t$item_tid.$memdef->{coll} = t$coll_tid.id",
+				 tight => 100,
+				 objects => Set::Object->new($coll, $item->object),
+				)
+			}
 
 		$item_id = $storage->id($item);
 
@@ -155,50 +167,50 @@ use Carp;
 
 sub new
 {
-   my ($pkg, $name, $alias) = @_;
-   bless [ $name, $alias ], $pkg;
+	my ($pkg, $name, $alias) = @_;
+	bless [ $name, $alias ], $pkg;
 }
 
 sub from
 {
-   my ($name, $alias) = @{shift()};
-   "$name t$alias"
+	my ($name, $alias) = @{shift()};
+	"$name t$alias"
 }
 
 sub where
 {
-   confess unless wantarray;
-   ()
+	confess unless wantarray;
+	()
 }
 package Tangram::CollOnDemand;
 
 sub TIESCALAR
 {
-   my $pkg = shift;
-   return bless [ @_ ], $pkg; # [ $type, $storage, $id, $member, $class ]
+	my $pkg = shift;
+	return bless [ @_ ], $pkg;	# [ $type, $storage, $id, $member, $class ]
 }
 
 sub FETCH
 {
-   my $self = shift;
-   my ($type, $def, $storage, $id, $member, $class) = @$self;
-   my $obj = $storage->{objects}{$id} or die;
-   my $coll = $type->demand($def, $storage, $obj, $member, $class);
-   untie $obj->{$member};
-   $obj->{$member} = $coll;
+	my $self = shift;
+	my ($type, $def, $storage, $id, $member, $class) = @$self;
+	my $obj = $storage->{objects}{$id} or die;
+	my $coll = $type->demand($def, $storage, $obj, $member, $class);
+	untie $obj->{$member};
+	$obj->{$member} = $coll;
 }
 
 sub STORE
 {
-   my ($self, $coll) = @_;
-   my ($type, $def, $storage, $id, $member, $class) = @$self;
+	my ($self, $coll) = @_;
+	my ($type, $def, $storage, $id, $member, $class) = @$self;
 
-   my $obj = $storage->{objects}{$id} or die;
-   $type->demand($def, $storage, $obj, $member, $class);
+	my $obj = $storage->{objects}{$id} or die;
+	$type->demand($def, $storage, $obj, $member, $class);
 
-   untie $obj->{$member};
+	untie $obj->{$member};
 
-   $obj->{$member} = $coll;
+	$obj->{$member} = $coll;
 }
 
 package Tangram::CollCursor;
@@ -207,25 +219,25 @@ package Tangram::CollCursor;
 
 sub build_select
 {
-   my ($self, $cols, $from, $where) = @_;
+	my ($self, $cols, $from, $where) = @_;
 
-   if ($self->{-coll_where})
-   {
-      $where .= ' AND ' if $where;
-      $where .= "$self->{-coll_where}" if $self->{-coll_where};
-   }
+	if ($self->{-coll_where})
+	{
+		$where .= ' AND ' if $where;
+		$where .= "$self->{-coll_where}" if $self->{-coll_where};
+	}
 
-   $where = $where && "WHERE $where";
-   $cols .= $self->{-coll_cols} if exists $self->{-coll_cols};
+	$where = $where && "WHERE $where";
+	$cols .= $self->{-coll_cols} if exists $self->{-coll_cols};
 	$from .= $self->{-coll_from} if exists $self->{-coll_from};
-   "SELECT $cols\n\tFROM $from\n\t$where";
+	"SELECT $cols\n\tFROM $from\n\t$where";
 }
 
 sub DESTROY
 {
-   my ($self) = @_;
-   #print "@{[ keys %$self ]}\n";
-   $self->{-storage}->free_table($self->{-coll_tid});
+	my ($self) = @_;
+	#print "@{[ keys %$self ]}\n";
+	$self->{-storage}->free_table($self->{-coll_tid});
 }
 
 package Tangram::BackRefOnDemand;
@@ -234,13 +246,13 @@ use base qw( Tangram::RefOnDemand );
 
 sub FETCH
 {
-   my $self = shift;
-   my ($storage, $id, $member, $refid) = @$self;
-   my $obj = $storage->{objects}{$id};
-   my $refobj = $storage->load($refid);
-   untie $obj->{$member};
-   $obj->{$member} = $refobj; # weak
-   return $refobj;
+	my $self = shift;
+	my ($storage, $id, $member, $refid) = @$self;
+	my $obj = $storage->{objects}{$id};
+	my $refobj = $storage->load($refid);
+	untie $obj->{$member};
+	$obj->{$member} = $refobj;	# weak
+	return $refobj;
 }
 
 package Tangram::BackRef;
@@ -251,28 +263,28 @@ $Tangram::Schema::TYPES{backref} = Tangram::BackRef->new;
 
 sub save
 {
-   () # do nothing; save is done by the collection
+	() # do nothing; save is done by the collection
 }
 
 sub read
 {
-   my ($self, $row, $obj, $members, $storage) = @_;
+	my ($self, $row, $obj, $members, $storage) = @_;
    
-   my $id = $storage->id($obj);
+	my $id = $storage->id($obj);
 
-   foreach my $r (keys %$members)
-   {
-      my $rid = shift @$row;
+	foreach my $r (keys %$members)
+	{
+		my $rid = shift @$row;
 
-      if ($rid)
-      {
-         tie $obj->{$r}, 'Tangram::BackRefOnDemand', $storage, $id, $r, $rid;
-      }
-      else
-      {
-         $obj->{$r} = undef;
-      }
-   }
+		if ($rid)
+		{
+			tie $obj->{$r}, 'Tangram::BackRefOnDemand', $storage, $id, $r, $rid;
+		}
+		else
+		{
+			$obj->{$r} = undef;
+		}
+	}
 }
 
 package Tangram::Alias;
@@ -281,7 +293,7 @@ my $top = 1_000;
 
 sub new
 {
-   ++$top
+	++$top
 }
 
 1;
