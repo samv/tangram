edd27031475ac9054747ae20384f107b6b8d6bcc
jll <jll>
2001-03-12 08:19:08 +0000

    deploy() and retreat() can take DBI connection parameters

--- Tangram/Relational.pm
+++ Tangram/Relational.pm
@@ -16,18 +16,37 @@ sub schema
 	return Tangram::Schema->new( @_ );
   }
 
+sub _with_handle {
+  my $method = shift;
+  my $schema = shift;
+
+  if (@_) {
+	my $arg = shift;
+
+	if (ref $arg) {
+	  Tangram::Relational::Engine->new($schema)->$method($arg)
+	} else {
+	  my $dbh = DBI->connect($arg, @_);
+	  eval { Tangram::Relational::Engine->new($schema)->$method($dbh) };
+	  $dbh->disconnect();
+  
+	  die $@ if $@;
+	}
+  } else {
+	Tangram::Relational::Engine->new($schema)->$method();
+  }
+}
+
 sub deploy
   {
-	shift; # class
-	my ($schema, $handle) = @_;
-	Tangram::Relational::Engine->new($schema)->deploy($handle);
+	shift;
+	_with_handle('deploy', @_);
   }
 
 sub retreat
   {
-	shift; # class
-	my ($schema, $handle) = @_;
-	Tangram::Relational::Engine->new($schema)->retreat($handle);
+	shift;
+	_with_handle('retreat', @_);
   }
 
 1;
