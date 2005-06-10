4539c55a923faf6399858315196dd2ae94ebc551
samv <samv>
2005-06-10 05:30:30 +0000

    Lots of Oracle enhancements

--- lib/Tangram/Oracle.pm
+++ lib/Tangram/Oracle.pm
@@ -17,7 +17,7 @@ sub connect
   }
 
 sub blob {
-    return "VARCHAR2(4000)";
+    return "CLOB";
 }
 
 sub date {
@@ -26,6 +26,35 @@ sub date {
 
 sub bool {
     return "INT(1)";
+}
+
+# Oracle--
+sub from_date {
+    $_[1];
+    #print STDERR "Converting FROM $_[1]\n";
+    #(my $date = $_[1]) =~ s{ }{T};
+    #$date;
+ }
+sub to_date {
+    $_[1];
+    #print STDERR "Converting TO $_[1]\n";
+    #(my $date = $_[1]) =~ s{T}{ };
+    #$date;
+}
+
+sub from_blob { $_[1] }
+sub to_blob { $_[1] }
+
+sub limit_sql {
+    my $self = shift;
+    my $spec = shift;
+    if ( ref $spec ) {
+	die unless ref $spec eq "ARRAY";
+	die "Oracle cannot handle two part limits"
+	    unless $spec->[0] eq "0";
+	$spec = pop @$spec;
+    }
+    return (postfilter => ["rownum <= $spec"]);
 }
 
 package Tangram::Oracle::Storage;
@@ -43,11 +72,12 @@ sub connect
     # Oracle doesn't really have a default date format (locale
     # dependant), so adjust it to use ISO-8601.
     $self->{db}->do
-	("ALTER SESSION SET NLS_DATE_FORMAT='YYYYMMDDHH24:MI:SS'");
+	("ALTER SESSION SET NLS_DATE_FORMAT='YYYY-MM-DD\"T\"HH24:MI:SS'");
     $self->{db}->do
 	("ALTER SESSION SET CONSTRAINTS = DEFERRED");
     $self->{db}->{RaiseError} = 1;
-    $self->{db}->{LongTruncOk} = 1;
+    $self->{db}->{LongTruncOk} = 0;
+    $self->{db}->{LongReadLen} = 1024*1024;
     return $self;
 }
 
