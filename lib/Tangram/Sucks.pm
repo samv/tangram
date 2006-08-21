f50abf0b19f7c1530a216ab434947186c142924d
Sam Vilain <sam@vilain.net>
2006-08-21 17:09:03 +0200

    Finish the sentences in Tangram::Sucks
    Also change various =over lists to headings so that more is indexed in
    the ToC.

--- lib/Tangram/Sucks.pm
+++ lib/Tangram/Sucks.pm
@@ -13,9 +13,7 @@ major problems that are inherant in the design, describe cases for
 which the Tangram metaphor does not work well, and list long standing
 TO-DO items.
 
-=head2 DESIGN CAVEATS
-
-=over
+=head1 DESIGN CAVEATS
 
 =item B<query language does not cover all SQL expressions>
 
@@ -33,30 +31,7 @@ Some people banter on about I<data access patterns>, which the Tangram
 schema represents.  But OO terms like that are usually treated as
 buzzwords anyway.
 
-=back
-
-=head2 HARD PROBLEMS
-
-=over
-
-=item B<partial column select>
-
-This optimisation has some serious dangers associated with it.
-
-It could either be
-
-=item B<no support for SQL UPDATE>
-
-It may be possible to write a version of C<$storage-E<gt>select()>
-that does this, which would look something like:
-
-  $storage->update
-      ( $r_object,
-        set => [ $r_object->{bar} == $r_object->{baz} + 2 ],
-        filter => ($r_object->{frop} != undef)
-      );
-
-=item B<no explicit support for re-orgs>
+=head2 re-orgs require a lot of core
 
 The situation where you have a large amount of schema reshaping to do,
 with a complex enough data structure can turn into a fairly difficult
@@ -68,63 +43,127 @@ on-demand autoloading combined with the automatic insertion of unknown
 objects will result in the entire database being loaded into core if
 it is sufficiently interlinked.
 
-=item B<replace SQL expression core>
+=head1 HARD PROBLEMS
+
+=head2 Partial Column Select
+
+When you partially select columns, you're breaking up your objects
+from nice identifiable blobs into scattered fragments of data.  It is
+quite easy to write code that breaks preconditions required for
+ACID-safe transactions.
+
+Nevertheless, you can do it;
+
+  $storage->select
+     ( undef,
+       filter => (...),
+       retrieve => [ $r_foo->{bar}, ... ]
+     );
+
+In principle you could make columns lazy, but this is only implemented
+for references and collections.
+
+=head1 MISSING FEATURES
+
+=head2 no support for SQL UPDATE
+
+It may be possible to write a version of C<$storage-E<gt>select()>
+that does this, which would look something like:
+
+  $storage->update
+      ( $r_object,
+        set => [ $r_object->{bar} == $r_object->{baz} + 2 ],
+        filter => ($r_object->{frop} != undef)
+      );
+
+=head2 replace SQL expression core
 
 The whole SQL expression core needs to be replaced with a SQL
 abstraction module that is a little better planned.  For instance,
 there should be placeholders used in a lot more places where the code
 just sticks in an integer etc.
 
-=item B<support for `large' collections>
+=head2 support for `large' collections
 
 Where it is impractical or undesirable to load all of a collection
 into memory, when you are adding a member and then updating the
-container, it should be possible to 
+container, it should be possible to lazily load individual members as
+required, rather than the whole lot.
 
-This could actually be achieved with a new Tangram::Type.
+This could actually be achieved with a new Tangram::Type.  This
+problem is particularly visible in the F<t/eg/RT.pm> example in the
+Tangram distribution - Queues, for instance, have a set of Tickets,
+but you almost never want to load all the tickets in for a queue.
+This means that you are creating a relation for the sake of querying,
+that is somewhat "dangerous" to actually load.
 
-=back
-
-=head2 MISSING FEATURES
-
-=over
-
-=item B<concise query expressions>
+=head2 concise query expressions
 
 For simple selects, this is too long:
 
-  ...
+  my $r_foo = $storage->remote("Foo");
+  my @bobs = $storage->select( $r_foo, $r_foo->{name} eq "Bob" );
 
-=item B<non-ID joins>
+=head2 non-ID joins
 
-=item B<tables with no primary key>
+Currently, all the L<Tangram::Type::Abstract::Coll>-based classes only
+support ID joins.
 
-=item B<tables with multi-column primary keys>
+=head2 tables with no primary key
 
-=item B<tables with auto_increment keys>
+You need to make a view for Tangram to access, a function to convert
+from the row contents to a (possibly hashed) integer 'primary' key.
+You also need to write update rules for the view.  It's a bit of a
+mess, but can be done.  DB-side C<md5_hex()> support can be used for
+this at a stretch.
 
-=item B<tables without a `type' column>
+=head2 tables with multi-column primary keys
 
-=item B<tables with custom `type' columns>
+Yeah.  Same thing as above; though normally just convert the
+multi-column key into an integer somehow.
 
-=item B<tables with implicit (presence) `type' columns>
+=head2 tables with auto_increment keys
 
-=item B<fully symmetric relationships>
+This is probably quite trivial to add, but nobody cared about it
+enough to add it yet.
 
-back-refs are read-only.
+=head2 tables without a type column
 
-=item B<bulk inserts>
+The 'type' column is de-normalized.  What it represents, in principle,
+is which tables that share the unique keyspace of this column have
+tuples present.  However, instead of being a bitmap or enum set
+column, it is an integer index into the perl-side schema.  This leaves
+something to be desired.
+
+=head2 tables with custom `type' columns
+
+Quite often you will come across schema that use enumerated or text
+columns to indicate typing information.  This is very difficult to map
+to Tangram inheritance, but again can be done with clever use of DB
+remapping features.
+
+=head2 tables with implicit (presence) `type' columns
+
+Apparently this is what David Wheeler's L<Object::Relational> does.
+Instead of having an explicit type indicator, the presence or absence
+of rows in other tables determines the result.  This approach uses a
+lot of outer joins, but perhaps that is a reasonable pay-off.
+
+=head2 fully symmetric relationships
+
+back-refs are read-only.  There is a complication here that the
+Perl-side objects need to also implement the conjoined nature of
+symmetric relationships.
+
+=head2 bulk inserts
 
 Inserting lots of similar objects should be more efficient.  Right now
 it generates a new STH for each object.
 
-=item B<`empty subclass' schema support>
+=head2 `empty subclass' schema support
 
 You should not need to explicitly add new classes to a schema if a
 superclass of them is already in the schema.
-
-=back
-
 
 =cut
 
