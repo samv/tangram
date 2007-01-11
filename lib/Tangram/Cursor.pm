samv/tangram

commit 36e28bfaab776dcbed9e57867e9b5ad2a9780731

RT#22696: avoid calling importer when we know we don't need to
The code used to call $storage->read_object on rows, even when we had
the original object in memory already, just so we can get the
"residue" columns right.  However, those are only required with
-retrieve, so if we're not doing a -retrieve, we can just drop the
columns.

lib/Tangram/Cursor.pm
@@ -286,12 +286,18 @@ sub _next
 
 	    my $class = $storage->{id2class}{$classId} or die "unknown class id $classId";
 
-	    # even if object is already loaded we must read it so that @rpw only contains residue
-	    my $obj = $storage->read_object($id, $class, $state);
+	    my $obj = $storage->{objects}{$id};
+
+	    # even if object is already loaded we must read it so
+	    # that @row only contains residue, with -retrieve
+	    if ( !defined($obj) or $self->{-retrieve} ) {
+		$obj = $storage->read_object($id, $class, $state);
+	    } else {
+		# just pretend we did it.
+		@row = ();
+	    }
 
 	    # if object is already loaded return previous copy
-	    $obj = $storage->{objects}{$id} if exists $storage->{objects}{$id};
-
 	    $self->{-current} = $obj;
 
 	} else {

commit d5fa585dc3549f447c87092b6a48bb4225c4918d

RT#22696: fix (probably harmless) warning
Regression from 36e28bfaab776dcbed9e57867e9b5ad2a9780731.  The cursors
generated internally by the collection loaders don't go through the
front door, so we don't know that they are selecting extra columns and
hence have to go through ->read_object().

lib/Tangram/Cursor.pm
@@ -288,11 +288,18 @@ sub _next
 
 	    my $obj = $storage->{objects}{$id};
 
-	    # even if object is already loaded we must read it so
-	    # that @row only contains residue, with -retrieve
-	    if ( !defined($obj) or $self->{-retrieve} ) {
+	    # even if object is already loaded we must read it so that
+	    # @row only contains residue, with -retrieve, and so that
+	    # any foreign/intrusive collections can get the
+	    # information they need.
+	    if ( !defined($obj) or $self->{-retrieve} or
+		 $self->{-no_skip_read} ) {
+		print $Tangram::TRACE __PACKAGE__.": reading object $id\n"
+		    if $Tangram::TRACE and $Tangram::DEBUG_LEVEL > 0;
 		$obj = $storage->read_object($id, $class, $state);
 	    } else {
+		print $Tangram::TRACE __PACKAGE__.": not reading object $id\n"
+		    if $Tangram::TRACE and $Tangram::DEBUG_LEVEL > 0;
 		# just pretend we did it.
 		@row = ();
 	    }

lib/Tangram/Type/Array/FromMany.pm
@@ -117,6 +117,7 @@ sub cursor
 	$cursor->{-coll_cols} = "t$coll_tid.$slot_col";
 	$cursor->{-coll_from} = "$table t$coll_tid";
 	$cursor->{-coll_where} = "t$coll_tid.$coll_col = $coll_id AND t$coll_tid.$item_col = t$item_tid.$storage->{schema}{sql}{id_col}";
+	$cursor->{-no_skip_read} = 1;
    
 	return $cursor;
 }

lib/Tangram/Type/Hash/FromMany.pm
@@ -139,6 +139,7 @@ sub cursor						# ?? factorize ??
     $cursor->{-coll_cols} = "t$coll_tid.$slot_col";
     $cursor->{-coll_from} = "$table t$coll_tid";
     $cursor->{-coll_where} = "t$coll_tid.$coll_col = $coll_id AND t$coll_tid.$item_col = t$item_tid.$storage->{schema}{sql}{id_col}";
+    $cursor->{-no_skip_read} = 1;
     
     return $cursor;
 }

lib/Tangram/Type/Set/FromMany.pm
@@ -132,6 +132,7 @@ sub demand
 		$cursor->{-coll_tid} = $coll_tid;
 		$cursor->{-coll_from} = "$table t$coll_tid";
 		$cursor->{-coll_where} = "t$coll_tid.$coll_col = $coll_id AND t$coll_tid.$item_col = t$item_tid.$storage->{schema}{sql}{id_col}";
+	    $cursor->{-no_skip_read} = 1;
 
 		$set->insert($cursor->select);
 	}